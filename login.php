bc40fc88e6e918b66721a3f2295da8cb686821ff
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2021-04-16 19:19:58 +0530

    Files Arrangement 2

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php
 <!DOCTYPE html>
 <html>
 
@@ -126,8 +127,8 @@
           <div class="row justify-content-center">
             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
               <h1 class="text-white">Create an account</h1>
-              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
-                free.</p>e a
+              <p class="text-lead text-white">Use theswesome forms to login or create new account in your project for
+                free.</p>
             </div>
           </div>
         </div>
@@ -231,3 +232,4 @@
 </body>
 
 </html>
+?>
